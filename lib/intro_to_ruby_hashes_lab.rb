33d367febf7e23930bce088b9fbf63479296c637
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-20 12:42:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,6 +7,10 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  if hash[key]
+    hash[key]
+  else
+    nil
 end
 
 def update_counting_hash(hash, key)
